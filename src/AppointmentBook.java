04017e47fa80e5d40b2a117ffc47a4926dc87d84
EricLi <ericl521@nycstudents.net>
2025-09-26 11:43:17 -0400

    Started working on AppointmentBook

---
+++ src/AppointmentBook.java
@@ -0,0 +1,14 @@
+public class AppointmentBook {
+    private boolean[][] schedule;
+
+    public AppointmentBook(boolean[][]schedule){
+        this.schedule = schedule;
+    }
+    private boolean isMinitureFree(int period, int minture){
+        return schedule[period-1][minture];
+    }
+    public int findFreeBlock(int period, int duration) {
+        return 0;
+    }
+}
+
