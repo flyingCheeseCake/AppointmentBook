864e7886d49dfac89e89da23d176e3304d94a09a
EricLi <ericl521@nycstudents.net>
2025-09-30 11:42:21 -0400

    Started working on AppointmentBook Working on findFreeBlock

--- src/AppointmentBook.java
+++ src/AppointmentBook.java
@@ -8,10 +8,26 @@ public class AppointmentBook {
         return schedule[period-1][minture];
     }
     public int findFreeBlock(int period, int duration) {
-        return 0;
+        int block = 0;
+
+        for(int i = 0; i < 60; i++)
+            if (isMinitureFree(period, i)){
+                block++;
+                if(block == duration){
+                    return i - duration + 1;
+                }
+                else block = 0;
+            }
+        return -1;
     }
     public boolean makeAppointment(int startPeriod, int endPeriod, int duration){
         return false;
     }
+    public void printPeriod(int period){
+        for (int i = 0; i < schedule[period-1].length; i++)
+         System.out.println(i + " " + schedule[period-1][i]);
+
+    }
+
 }
 
