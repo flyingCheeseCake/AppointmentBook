8828ab18a4dcdeee18d763c348d7f86b7b95bb65
flyingCheeseCake <ericl521@nycstudents.net>
2025-11-06 11:44:33 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,15 @@ public class Main {
         boolean[][] schedule = new boolean[8][60];
         AppointmentBook a = new AppointmentBook(schedule);
         for (int i = 10; i < 15; i++) schedule[1][i] = true;
-
+        for (int i = 15; i < 30; i++) schedule[1][i] = false;
+        for (int i = 0; i < 10; i++) schedule[1][i] = false;
+        for (int i = 30; i < 45; i++) schedule[1][i] = true;
+        for (int i = 45; i < 50; i++) schedule[1][i] = false;
+        for (int i = 50; i < 60; i++) schedule[1][i] = false;
         a.printPeriod(2);
-        a.findFreeBlock(2, 15);
-        a.findFreeBlock(2, 20);
-        a.findFreeBlock(2, 9);
+        System.out.println(a.findFreeBlock(2, 15));
+        System.out.println(a.findFreeBlock(2, 20));
+        System.out.println(a.findFreeBlock(2, 9));
         schedule = new boolean[8][60];
 
         AppointmentBook b = new AppointmentBook(schedule);
@@ -22,8 +26,15 @@ public class Main {
         System.out.println(b.makeAppointment(2, 4, 22));
         System.out.println(b.makeAppointment(3, 4, 3));
         System.out.println(b.makeAppointment(2, 4, 30));
-
         b.printPeriod(4);
+
+        for (int i = 25; i < 30; i++) schedule[1][i] = true;
+        for (int i = 0; i < 15; i++) schedule[2][i] = true;
+        for (int i = 41; i < 60; i++) schedule[2][i] = true;
+        for (int i = 5; i < 30; i++) schedule[3][i] = true;
+        for (int i = 44; i < 60; i++) schedule[3][i] = true;
+
+
         int period = 2;
         while (period < 5) {
             System.out.println("Period:" + period);
